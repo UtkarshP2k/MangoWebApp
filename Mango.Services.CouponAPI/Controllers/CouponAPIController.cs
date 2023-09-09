4d6c70b45513e1cf1ae1d79360661438aeb280b9
UtkarshP2k <panwalkarutkarsh@gmail.com>
2023-09-09 18:39:09 +0530

    initial commit

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,63 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private ResponseDto _responseDto;
+
+        public CouponAPIController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _responseDto = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                _responseDto.Result = _dbContext.Coupons.ToList();
+                return _responseDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.Message = ex.Message;
+                _responseDto.IsSuccess = false;
+
+            }
+            return _responseDto;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                var coupon = _dbContext.Coupons.FirstOrDefault(x => x.CouponId == id);
+                if(coupon == null)
+                {
+                    _responseDto.IsSuccess = false;
+                    _responseDto.Message = "No coupon found with this id!";
+                }
+
+                _responseDto.Result = coupon;
+                return _responseDto;
+            }
+            catch (Exception ex)
+            {
+                _responseDto.Message = ex.Message;
+                _responseDto.IsSuccess = false;
+
+            }
+            return _responseDto;
+        }
+    }
+}
--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -1,8 +1,15 @@
+using Mango.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,5 +28,19 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigrations();
 app.Run();
+
+
+void ApplyMigrations()
+{
+
+    using var scope = app.Services.CreateScope();
+
+    var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (_db.Database.GetPendingMigrations().Any())
+    {
+        _db.Database.Migrate();
+    }
+}
